7af5a058da663428ced80a2b6f05dcb40e7f3838
Åke Engelbrektson <eson57@users.noreply.github.com>
2026-09-17 07:55:34 +0200

    Create harbour-spliit-sv.ts
    Add Swedish translation

---
+++ translations/harbour-spliit-sv.ts
@@ -0,0 +1,395 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="page.error.default_text">
+        <source>There was an error, please try again later or contact the developer with details about what you were doing.</source>
+        <translation>Det uppstod ett fel, försök igen senare eller kontakta utvecklaren med information om vad du gjorde.</translation>
+    </message>
+    <message id="page.init.initialization_failed">
+        <source>Initialization Failed</source>
+        <translation>Kunde inte starta</translation>
+    </message>
+    <message id="page.init.initialization_failed_description">
+        <source>The initialization of the app failed. There&apos;s not much you can do about it except contacting the developer.</source>
+        <translation>Appstarten misslyckades. Det finns inte mycket du kan göra åt det förutom att kontakta utvecklaren.</translation>
+    </message>
+    <message id="group_selector.title">
+        <source>Select a Group</source>
+        <translation>Väljen grupp</translation>
+    </message>
+    <message id="group_selector.no_groups">
+        <source>There are no groups saved. You can use the pull-down menu to add a new one.</source>
+        <translation>Det finns inga grupper. Du kan använda toppmenyn för att lägga till en.</translation>
+    </message>
+    <message id="add_group.help_text">
+        <source>Please provide the group ID or share link below.</source>
+        <oldsource>Please provide the group ID below.</oldsource>
+        <translation>Ange grupp-ID eller delningslänk nedan.</translation>
+    </message>
+    <message id="add_group.group_id_field.label">
+        <source>Group ID</source>
+        <translation>Grupp-ID</translation>
+    </message>
+    <message id="group_selector.add_group">
+        <source>Select existing group</source>
+        <oldsource>Add a group</oldsource>
+        <translation>Välj befintlig grupp</translation>
+    </message>
+    <message id="group_selector.invalid_group_provided">
+        <source>Invalid group provided.</source>
+        <translation>Ogiltig grupp angiven.</translation>
+    </message>
+    <message id="add_group.error.generic">
+        <source>There was an error: %1</source>
+        <translation>Ett fell uppstod: %1</translation>
+    </message>
+    <message id="add_group.error.not_found">
+        <source>The group does not exist.</source>
+        <translation>Gruppen finns inte.</translation>
+    </message>
+    <message id="add_group.group_name_label">
+        <source>Group name: %1</source>
+        <translation>Gruppnamn: %1</translation>
+    </message>
+    <message id="global.loading">
+        <source>Loading...</source>
+        <translation>Läser in...</translation>
+    </message>
+    <message id="group_selector.failed_fetching">
+        <source>Failed fetching list of groups, please  try again later.</source>
+        <translation>Kunde inte hämta grupplista. Försök igen senare.</translation>
+    </message>
+    <message id="group_selector.stored_groups">
+        <source>Saved Groups</source>
+        <translation>Sparade grupper</translation>
+    </message>
+    <message id="group_detail.change_group">
+        <source>Groups</source>
+        <oldsource>Change group</oldsource>
+        <translation>Grupper</translation>
+    </message>
+    <message id="global.remove">
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message id="add_group.error.fetch_more">
+        <source>Failed fetching more expenses from the api</source>
+        <translation>Kunde inte hämta fler utgifter från API:et</translation>
+    </message>
+    <message id="group_detail.no_expenses">
+        <source>Wouldn&apos;t you look at that, no expenses yet! Why don&apos;t you create your first?</source>
+        <translation>Ser man på, inga utgifter än! Varför skapar du inte din första?</translation>
+    </message>
+    <message id="group_detail.add_expense">
+        <source>Add expense</source>
+        <translation>Lägg till utgift</translation>
+    </message>
+    <message id="global.cancel">
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message id="global.add">
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message id="add_expense.field.name">
+        <source>Title</source>
+        <extracomment>Title of the expense</extracomment>
+        <translation>Utgiftsnamn</translation>
+    </message>
+    <message id="add_expense.field.date">
+        <source>Expense date</source>
+        <oldsource>&lt;strong&gt;Expense date&lt;/strong&gt;: %1</oldsource>
+        <translation>Utgiftsdatum</translation>
+    </message>
+    <message id="add_expense.error.category_fetching_failed">
+        <source>Fetching categories failed, only the default category will be available.</source>
+        <translation>Kunde inte hämta kategorier, endast standardkategorin finns tillgänglig.</translation>
+    </message>
+    <message id="add_expense.field.category">
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message id="category_select.title">
+        <source>Choose a category</source>
+        <translation>Välj en kategori</translation>
+    </message>
+    <message id="add_expense.field.currency">
+        <source>Currency</source>
+        <translation>Valuta</translation>
+    </message>
+    <message id="currency_select.title">
+        <source>Choose currency</source>
+        <translation>Välj valuta</translation>
+    </message>
+    <message id="add_expense.field.amount">
+        <source>Amount (%1)</source>
+        <oldsource>Amount</oldsource>
+        <translation>Summa (%1)</translation>
+    </message>
+    <message id="add_expense.field.amount_no_currency">
+        <source>Amount</source>
+        <translation>Summa</translation>
+    </message>
+    <message id="add_expense.field.paid_by">
+        <source>Paid by</source>
+        <oldsource>Paid By</oldsource>
+        <translation>Betalad av</translation>
+    </message>
+    <message id="participant_select.title">
+        <source>Choose a participant</source>
+        <translation>Välj en deltagare</translation>
+    </message>
+    <message id="global.save">
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message id="settings.section.app">
+        <source>App Settings</source>
+        <translation>Appinställningar</translation>
+    </message>
+    <message id="settings.language.auto">
+        <source>Automatic</source>
+        <extracomment>As in automatic language selection</extracomment>
+        <translation>Automatisk</translation>
+    </message>
+    <message id="settings.language.label">
+        <source>Language</source>
+        <translation>Språk</translation>
+    </message>
+    <message id="global.settings">
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="settings.current_participant">
+        <source>Active user</source>
+        <translation>Aktiv användare</translation>
+    </message>
+    <message id="add_expense.field.is_reimbursement">
+        <source>This is a reimbursement</source>
+        <translation>Detta är en återbetalning</translation>
+    </message>
+    <message id="add_expense.field.notes">
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+    <message id="add_expense.label.paid_for">
+        <source>Paid for</source>
+        <translation>Betalad för</translation>
+    </message>
+    <message id="group_detail.create_failed">
+        <source>Creating the expense failed: %1</source>
+        <translation>Kunde inte skapa utgiften: %1</translation>
+    </message>
+    <message id="participant.unknown">
+        <source>Unknown</source>
+        <extracomment>Paid by unknown participant
+----------
+Paid for unknown participant
+----------
+Unknown participant owes money
+----------
+Unknown participant is owed money</extracomment>
+        <translation>Okänd</translation>
+    </message>
+    <message id="expense_row.paid_by_for_label">
+        <source>Paid by &lt;strong&gt;%1&lt;/strong&gt; for &lt;strong&gt;%2&lt;/strong&gt;</source>
+        <translation>Betalad av &lt;strong&gt;%1&lt;/strong&gt; för &lt;strong&gt;%2&lt;/strong&gt;</translation>
+    </message>
+    <message id="expense_row.balance">
+        <source>Your balance: &lt;strong&gt;%1&lt;/strong&gt; (%2 - %3)</source>
+        <oldsource>Your balance: &lt;strong&gt;%1&lt;/strong&gt;</oldsource>
+        <extracomment>Your balance: &lt;strong&gt;€ 2.00&lt;/strong&gt; (€3.00 - €1.00)</extracomment>
+        <translation>Din balansräkning: &lt;strong&gt;%1&lt;/strong&gt; (%2 - %3)</translation>
+    </message>
+    <message id="expense_row.error.delete">
+        <source>Failed deleting the item: %1</source>
+        <translation>Kunde inte ta bort posten: %1</translation>
+    </message>
+    <message id="add_expense.confirm_text">
+        <source>Update expense</source>
+        <translation>Uppdatera utgiften</translation>
+    </message>
+    <message id="add_expense.error.fetching_failed">
+        <source>Fetching the expense failed: %1</source>
+        <translation>Kunde inte hämta utgiften: %1</translation>
+    </message>
+    <message id="add_expense.error.unsupported_multi_currency">
+        <source>Warning: This expense is in a non-default currency, this app does not support it yet. Please don&apos;t save anything.</source>
+        <translation>Varning! Den här utgiften är i en annan valuta än standardvalutan, den här appen stödjer det inte än. Spara inte något.</translation>
+    </message>
+    <message id="group_detail.error.updating_failed">
+        <source>Updating the item failed: %1</source>
+        <translation>Kunde inte uppdatera posten: %1</translation>
+    </message>
+    <message id="group_selector.create_group">
+        <source>Create group</source>
+        <translation>Skapa grupp</translation>
+    </message>
+    <message id="page.init.currency_initialization_failed">
+        <source>Currency Initialization Failed</source>
+        <translation>Kunde inte initiera valuta</translation>
+    </message>
+    <message id="page.init.currency_initialization_failed_description">
+        <source>Currency formatting libraries failed to load. Please reinstall the app or contact the developer.</source>
+        <translation>Bibliotek för valutahantering kunde inte läsas in. Installera om appen eller kontakta utvecklaren.</translation>
+    </message>
+    <message id="settings.section.group">
+        <source>Group Settings</source>
+        <translation>Gruppinställningar</translation>
+    </message>
+    <message id="settings.group_name">
+        <source>Group name</source>
+        <translation>Gruppnamn</translation>
+    </message>
+    <message id="settings.currency">
+        <source>Main currency</source>
+        <translation>Huvudvaluta</translation>
+    </message>
+    <message id="settings.group_information">
+        <source>Group information</source>
+        <translation>Gruppinformation</translation>
+    </message>
+    <message id="settings.participants">
+        <source>Participants</source>
+        <translation>Deltagare</translation>
+    </message>
+    <message id="settings.add_participant">
+        <source>Add participant</source>
+        <translation>Lägg till deltagare</translation>
+    </message>
+    <message id="group_detail.error.group_update">
+        <source>Failed updating group: %1</source>
+        <translation>Kunde inte uppdatera gruppen: %1</translation>
+    </message>
+    <message id="add_expense.error.unsupported_split_mode">
+        <source>Warning: This item uses a split mode that&apos;s currently not supported by the app. If you save this item, the split mode will be changed to even shares.</source>
+        <translation>Varning! Den här posten använder ett delat läge som för närvarande inte stöds av appen. Om du sparar den här posten kommer det delade läget att ändras till jämna andelar.</translation>
+    </message>
+    <message id="balances.title">
+        <source>Balances</source>
+        <translation>Balansräkningar</translation>
+    </message>
+    <message id="balances.error.fetching">
+        <source>Failed fetching list of balances: %1</source>
+        <translation>Kunde inte hämta balanslistan: %1</translation>
+    </message>
+    <message id="balances.description">
+        <source>This is the amount that each participant paid or was paid for.</source>
+        <translation>Det här är beloppet som varje deltagare betalade eller fick betalt.</translation>
+    </message>
+    <message id="balances.reimbursements">
+        <source>Suggested reimbursements</source>
+        <translation>Föreslagna ersättningar</translation>
+    </message>
+    <message id="balances.reimbursements.owing_text">
+        <source>&lt;strong&gt;%1&lt;/strong&gt; owes &lt;strong&gt;%2&lt;/strong&gt;</source>
+        <translation>&lt;strong&gt;%1&lt;/strong&gt; är skyldig &lt;strong&gt;%2&lt;/strong&gt;</translation>
+    </message>
+    <message id="balances.reimbursements.add_expense_name">
+        <source>Reimbursement</source>
+        <extracomment>The name of a new expense when created from the &quot;Mark as paid&quot; button on the balances/reimbursements page</extracomment>
+        <translation>Återbetalning</translation>
+    </message>
+    <message id="balances.reimbursement_created">
+        <source>The reimbursement has been successfully created.</source>
+        <translation>Återbetalningen har skapats.</translation>
+    </message>
+    <message id="balances.reimbursement_create_failed">
+        <source>Creating the reimbursement failed: %1</source>
+        <translation>Kunde inte skapa återbetalning: %1</translation>
+    </message>
+    <message id="balances.no_balances">
+        <source>Nobody owes anyone anything—isn&apos;t that nice? But it also means there&apos;s nothing to display on this page.</source>
+        <translation>Ingen är skyldig någon någontingt, är inte det bra? Men det betyder också att det inte finns något att visa på den här sidan.</translation>
+    </message>
+    <message id="group_selector.share_group_url">
+        <source>Share URL</source>
+        <translation>Delnings-URL</translation>
+    </message>
+    <message id="group_selector.share_group.title">
+        <source>Spliit group %1</source>
+        <translation>Spliit-grupp %1</translation>
+    </message>
+    <message id="group_selector.share_group.sharer_title">
+        <source>Share group %1 link</source>
+        <translation>Dela grupp %1 länk</translation>
+    </message>
+    <message id="global.create">
+        <source>Create</source>
+        <translation>Skapa</translation>
+    </message>
+    <message id="group_selector.error.add_group">
+        <source>There was an error creating the group: %1</source>
+        <translation>Kunde inte skapa gruppen: %1</translation>
+    </message>
+    <message id="balances.error.stats_fetching">
+        <source>Failed fetching stats: %1</source>
+        <translation>Kunde inte hämta statistik: %1</translation>
+    </message>
+    <message id="balances.stats">
+        <source>Stats</source>
+        <translation>Statistik</translation>
+    </message>
+    <message id="balances.stats.total_spendings">
+        <source>Total group spendings</source>
+        <translation>Grupputgifter totalt</translation>
+    </message>
+    <message id="balances.stats.total_spendings_participant">
+        <source>Your total spendings</source>
+        <oldsource>Total participant spendings</oldsource>
+        <translation>Dina utgifter totalt</translation>
+    </message>
+    <message id="balances.stats.total_share_participant">
+        <source>Your total share</source>
+        <translation>Din del</translation>
+    </message>
+    <message id="activities.title">
+        <source>Activities</source>
+        <translation>Aktiviteter</translation>
+    </message>
+    <message id="activities.error.fetching">
+        <source>There was an error while fetching activities: %1</source>
+        <translation>Kunde inte hämta aktiviteter: %1</translation>
+    </message>
+    <message id="group_detail.activities">
+        <source>Activities</source>
+        <translation>Aktiviteter</translation>
+    </message>
+    <message id="activity.update_group">
+        <source>Group was updated by &lt;strong&gt;%1&lt;/strong&gt;.</source>
+        <translation>Gruppen uppdaterades av &lt;strong&gt;%1&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.create_expense.with_name">
+        <source>Expense &lt;i&gt;%1&lt;/i&gt; was created by &lt;strong&gt;%2&lt;/strong&gt;.</source>
+        <oldsource>Expense &lt;i&gt;%1&lt;/i&gt; was created by &lt;strong&gt;%2&lt;/strong&gt;</oldsource>
+        <translation>Utgiften &lt;i&gt;%1&lt;/i&gt; skapades av &lt;strong&gt;%2&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.create_expense.no_name">
+        <source>Expense was created by &lt;strong&gt;%1&lt;/strong&gt;.</source>
+        <translation>Utgift skapades av &lt;strong&gt;%1&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.unknown">
+        <source>Unknown activity type</source>
+        <translation>Okänd aktivitetstyp</translation>
+    </message>
+    <message id="activity.update_expense.with_name">
+        <source>Expense &lt;i&gt;%1&lt;/i&gt; was updated by &lt;strong&gt;%2&lt;/strong&gt;.</source>
+        <translation>Utgiften &lt;i&gt;%1&lt;/i&gt; uppdaterades av &lt;strong&gt;%2&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.update_expense.no_name">
+        <source>Expense was updated by &lt;strong&gt;%1&lt;/strong&gt;.</source>
+        <translation>Utgift uppdaterades av &lt;strong&gt;%1&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.delete_expense.with_name">
+        <source>Expense &lt;i&gt;%1&lt;/i&gt; was deleted by &lt;strong&gt;%2&lt;/strong&gt;.</source>
+        <translation>Utgiften &lt;i&gt;%1&lt;/i&gt; togs bort av &lt;strong&gt;%2&lt;/strong&gt;.</translation>
+    </message>
+    <message id="activity.delete_expense.no_name">
+        <source>Expense was deleted by &lt;strong&gt;%1&lt;/strong&gt;.</source>
+        <translation>Utgift togs bort av &lt;strong&gt;%1&lt;/strong&gt;.</translation>
+    </message>
+</context>
+</TS>
